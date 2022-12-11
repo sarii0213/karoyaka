86071bce9b299e0db0fa77489051dd308e21f0d5
sarii0213 <anjing.singka@gmail.com>
2022-12-11 17:22:37 +0900

    :tada: deviseの導入
    ログイン機能、サインイン機能、ログアウト機能のコントローラ・ビュー・ルーティングの大枠を作成
    routes.rbのアノテーションもシステムのルーティングは省略するよう設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,44 @@
+# == Route Map
+#
+#                                   Prefix Verb   URI Pattern                                                                                       Controller#Action
+#                         new_user_session GET    /users/sign_in(.:format)                                                                          users/sessions#new
+#                             user_session POST   /users/sign_in(.:format)                                                                          users/sessions#create
+#                     destroy_user_session DELETE /users/sign_out(.:format)                                                                         users/sessions#destroy
+#                        new_user_password GET    /users/password/new(.:format)                                                                     devise/passwords#new
+#                       edit_user_password GET    /users/password/edit(.:format)                                                                    devise/passwords#edit
+#                            user_password PATCH  /users/password(.:format)                                                                         devise/passwords#update
+#                                          PUT    /users/password(.:format)                                                                         devise/passwords#update
+#                                          POST   /users/password(.:format)                                                                         devise/passwords#create
+#                 cancel_user_registration GET    /users/cancel(.:format)                                                                           users/registrations#cancel
+#                    new_user_registration GET    /users/sign_up(.:format)                                                                          users/registrations#new
+#                   edit_user_registration GET    /users/edit(.:format)                                                                             users/registrations#edit
+#                        user_registration PATCH  /users(.:format)                                                                                  users/registrations#update
+#                                          PUT    /users(.:format)                                                                                  users/registrations#update
+#                                          DELETE /users(.:format)                                                                                  users/registrations#destroy
+#                                          POST   /users(.:format)                                                                                  users/registrations#create
+#                                     user GET    /users/:id(.:format)                                                                              users#show
+#                                   signup GET    /signup(.:format)                                                                                 users/registrations#new
+#                                    login GET    /login(.:format)                                                                                  users/sessions#new
+#                                   logout GET    /logout(.:format)                                                                                 users/sessions#destroy
+#                                     root GET    /                                                                                                 samples#index
+#                              sidekiq_web        /sidekiq                                                                                          Sidekiq::Web
+
 Rails.application.routes.draw do
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, :controllers => {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  resources :users, only: [:show]
+
+  devise_scope :user do
+    get 'signup', to: 'users/registrations#new'
+    get 'login', to: 'users/sessions#new'
+    get 'logout', to: 'users/sessions#destroy'
+  end
+
   root to: 'samples#index'
 
   if Rails.env.development?
