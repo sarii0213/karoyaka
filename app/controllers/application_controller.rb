a7f97a46f26fd0ca3fdf5c6b738610f3de764f32
sarii0213 <anjing.singka@gmail.com>
2022-12-11 17:34:52 +0900

    :shirt: rubocop & rspec のメッセージをもとにコード修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     user_path(id: current_user.id)
   end
 end
