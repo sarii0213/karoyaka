9319337faed667bafb5846be3134ae71205d23cf
sarii0213 <anjing.singka@gmail.com>
2022-12-30 17:08:18 +0900

    :heavy_plus_sign: categoryモデルのシードデータを作成

---
+++ db/fixtures/01_category.rb
@@ -0,0 +1,58 @@
+Category.seed(:name, [
+  {
+    name: '生活雑貨',
+    description: '使い捨てのもの、掃除グッズ、洗面グッズ、文房具、ショッパー'
+  },
+  {
+    name: 'ファッション小物',
+    description: 'バッグ、帽子、香水、靴、アクセサリー'
+  },
+  {
+    name: '美容グッズ',
+    description: 'スキンケア、コスメ、ボディケア、ネイル、入浴剤'
+  },
+  {
+    name: '洋服・布類',
+    description: '靴下、下着、上着、タオル、ハンカチ'
+  },
+  {
+    name: '紙類・書類',
+    description: 'レシート、ポイントカード、ノート、手紙'
+  },
+  {
+    name: '書籍・CD/DVD',
+    description: '実用書、映画、思い出が詰まったアルバム'
+  },
+  {
+    name: '家具',
+    description: 'ソファ、椅子、収納グッズ'
+  },
+  {
+    name: 'キッチン小物',
+    description: '食器、調理器具'
+  },
+  {
+    name: '電子機器',
+    description: 'コード、充電器'
+  },
+  {
+    name: '思い出の品',
+    description: 'もらったお土産・プレゼント、記念品、アルバム'
+  },
+  {
+    name: '食品',
+    description: '調味料、レトルト'
+  },
+  {
+    name: 'スポーツ用品',
+    description: 'ラケット、水着、エアロバイク、ステッパー'
+  },
+  {
+    name: 'インテリア系',
+    description: '置物、観葉植物'
+  },
+  {
+    name: 'デジタル',
+    description: 'スマホアプリ、メール、連絡先、写真'
+  },
+])
