8e184ed73ac2bce16422d9bf5319d43c539f6a37
sarii0213 <anjing.singka@gmail.com>
2022-12-30 16:45:21 +0900

    :white_check_mark: categoryモデルのfactoryとmodel specを作成

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :bigint           not null, primary key
+#  description :text(65535)      not null
+#  name        :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_categories_on_name  (name) UNIQUE
+#
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  subject { build(:category) }
+  it { is_expected.to be_valid }
+end
