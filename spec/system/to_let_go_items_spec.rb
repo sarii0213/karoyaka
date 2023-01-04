c173508e9c8b2952e1e95c17f06656fef96dfd30
sarii0213 <anjing.singka@gmail.com>
2023-01-04 19:10:06 +0900

    :+1: to_let_go_itemsにrename

--- spec/system/to_let_go_items_spec.rb
+++ spec/system/to_let_go_items_spec.rb
@@ -8,22 +8,22 @@ RSpec.describe "手放したいものリスト", type: :system do
 
   describe '新規登録' do
     it '手放したいものを新規登録できること' do
-      visit new_to_let_go_list_path
-      attach_file 'to_let_go_list[image]', Rails.root.join('spec', 'fixtures', 'dummy.png')
-      find('#to_let_go_list_category_id').find("option[value='2']").select_option
-      fill_in 'to_let_go_list[item]', with: 'セーター'
-      find('#to_let_go_list_reason_id').find("option[value='2']").select_option
+      visit new_to_let_go_item_path
+      attach_file 'to_let_go_item[image]', Rails.root.join('spec', 'fixtures', 'dummy.png')
+      find('#to_let_go_item_category_id').find("option[value='2']").select_option
+      fill_in 'to_let_go_item[name]', with: 'セーター'
+      find('#to_let_go_item_reason_id').find("option[value='2']").select_option
       click_on '登録する'
       expect(page).to have_content '登録しました'
     end
   end
 
   describe '編集' do
-    let!(:to_let_go_list) { create(:to_let_go_list, user: user) }
+    let!(:to_let_go_item) { create(:to_let_go_item, user: user) }
     it '編集ができること' do
-      visit to_let_go_list_path(to_let_go_list)
+      visit to_let_go_item_path(to_let_go_item)
       click_on '編集する'
-      fill_in 'to_let_go_list[item]', with: 'スウェット'
+      fill_in 'to_let_go_item[name]', with: 'スウェット'
       click_on '更新する'
       expect(page).to have_content '更新しました'
       expect(page).to have_content 'スウェット'
@@ -31,12 +31,12 @@ RSpec.describe "手放したいものリスト", type: :system do
   end
 
   describe '削除' do
-    let!(:to_let_go_list) { create(:to_let_go_list, user: user) }
+    let!(:to_let_go_item) { create(:to_let_go_item, user: user) }
     it '削除ができること' do
-      visit to_let_go_list_path(to_let_go_list)
+      visit to_let_go_item_path(to_let_go_item)
       accept_confirm { click_on '削除' }
       expect(page).to have_content '削除しました'
-      expect(page).not_to have_content to_let_go_list.category.name
+      expect(page).not_to have_content to_let_go_item.category.name
     end
   end
 
