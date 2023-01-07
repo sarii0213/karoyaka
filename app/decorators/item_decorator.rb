f6b232acb56e0924e15d85d12242614c7a95c42c
sarii0213 <anjing.singka@gmail.com>
2023-01-07 21:23:54 +0900

    :shirt: exec rubocop

--- app/decorators/item_decorator.rb
+++ app/decorators/item_decorator.rb
@@ -1,7 +1,7 @@
 class ItemDecorator < Draper::Decorator
   delegate_all
 
-  def img(size = :origin)
+  def img(size)
     return 'dummy-100X100.png' unless image.attached?
 
     command = case size
@@ -9,12 +9,9 @@ class ItemDecorator < Draper::Decorator
                 { resize_to_fill: [100, 100] }
               when :lg
                 { resize_to_fill: [200, 200] }
-              else
-                false
               end
 
     image_url = command ? image.variant(command).processed : image
     h.rails_storage_proxy_url(image_url, only_path: true)
   end
-
 end
