b4ded155ff995636fc98424c1cae8de89e1d5950
sarii0213 <anjing.singka@gmail.com>
2023-01-12 10:59:14 +0900

    :heavy_plus_sign: Quoteモデルのシードを作成

---
+++ db/fixtures/05_quote.rb
@@ -0,0 +1,34 @@
+Quote.seed(:id,
+           {
+             id: 1,
+             author: 'スナフキン',
+             content: '物の持ち過ぎで苦しむのは、自分だぞ'
+           },
+           {
+             id: 2,
+             author: '近藤麻理恵',
+             content: '「いつか使う」の「いつか」は永遠にこない'
+           },
+           {
+             id: 3,
+             author: 'グレッグ・マキューン',
+             content: '「本当に重要なのは何か？」それ以外のことは、全部捨てていい'
+           },
+           {
+             id: 4,
+             content: 'その服、好きな人の前でも着られますか？'
+           },
+           {
+             id: 5,
+             author: 'minimalist takeru',
+             content: '何かを所有する度に、必ず失うものがある',
+             description: '失うもの：購入代金、時間、空間、家賃、サブスク費用、メンテナンス時間、探し物をする時間など。得られるものだけではなく、失うものにも目を向けよう。(https://youtu.be/Ewawu-BoyLU?t=642)'
+           },
+           {
+             id: 6,
+             author: 'minimalist takeru',
+             content: 'モノの持ちすぎは人生の重荷',
+             description: '(https://youtu.be/Ewawu-BoyLU?t=1862)'
+           }
+           )
+
