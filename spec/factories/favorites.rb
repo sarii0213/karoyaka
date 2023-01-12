2eddf4700030f7564ef386ec3c7f524243d27b0e
sarii0213 <anjing.singka@gmail.com>
2023-01-12 13:37:59 +0900

    :sparkles: Favoriteモデルのfactoryとspecを作成

---
+++ spec/factories/favorites.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: favorites
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  quote_id   :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_favorites_on_quote_id              (quote_id)
+#  index_favorites_on_quote_id_and_user_id  (quote_id,user_id) UNIQUE
+#  index_favorites_on_user_id               (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (quote_id => quotes.id)
+#  fk_rails_...  (user_id => users.id)
+#
+FactoryBot.define do
+  factory :favorite do
+    user
+    quote
+  end
+end
