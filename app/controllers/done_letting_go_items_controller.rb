3517fa8fea3a98be48f8f79015f5ecc406c36275
sarii0213 <anjing.singka@gmail.com>
2023-01-16 20:34:56 +0900

    :shirt: exec rubocop

--- app/controllers/done_letting_go_items_controller.rb
+++ app/controllers/done_letting_go_items_controller.rb
@@ -8,9 +8,7 @@ class DoneLettingGoItemsController < ApplicationController
   end
 
   def new
-    if params[:item_id]
-      @to_let_go_item = ToLetGoItem.find(params[:item_id])
-    end
+    @to_let_go_item = ToLetGoItem.find(params[:item_id]) if params[:item_id]
     @done_letting_go_item = DoneLettingGoItem.new
   end
 
@@ -18,25 +16,24 @@ class DoneLettingGoItemsController < ApplicationController
     @done_letting_go_item = current_user.done_letting_go_items.find(params[:id])
   end
 
+  # rubocop:disable  Metrics/MethodLength, Metrics/AbcSize
   def create
     @done_letting_go_item = current_user.done_letting_go_items.build(done_letting_go_item_params)
     if params[:done_letting_go_item][:item_id] && done_letting_go_item_params[:image].blank?
       carried_item = ToLetGoItem.find(params[:done_letting_go_item][:item_id])
-      if carried_item.image.attached?
-        carried_item.image.blob.open do |tmp|
-          @done_letting_go_item.image.attach(io: File.open(tmp.path), filename: carried_item.image.blob.filename)
-        end
-      end
+      duplicate_image(carried_item)
     end
     if @done_letting_go_item.save
       carried_item&.destroy!
       redirect_to done_letting_go_item_path(@done_letting_go_item), notice: '手放し済みリストに登録しました'
     elsif params[:done_letting_go_item][:item_id]
-      redirect_to new_done_letting_go_item_path(item_id: params[:done_letting_go_item][:item_id]), flash: { error: @done_letting_go_item.errors.full_messages }
+      redirect_to new_done_letting_go_item_path(item_id: params[:done_letting_go_item][:item_id]),
+                  flash: { error: @done_letting_go_item.errors.full_messages }
     else
       render :new, status: :unprocessable_entity
     end
   end
+  # rubocop:enable  Metrics/MethodLength, Metrics/AbcSize
 
   def update
     @done_letting_go_item = current_user.done_letting_go_items.find(params[:id])
@@ -57,5 +54,13 @@ class DoneLettingGoItemsController < ApplicationController
 
   def done_letting_go_item_params
     params.require(:done_letting_go_item).permit(:image, :category_id, :name, :reason_id, :letting_go_way_id)
+  end
+
+  def duplicate_image(item)
+    return unless item.image.attached?
+
+    item.image.blob.open do |tmp|
+      @done_letting_go_item.image.attach(io: File.open(tmp.path), filename: item.image.blob.filename)
+    end
   end
 end
