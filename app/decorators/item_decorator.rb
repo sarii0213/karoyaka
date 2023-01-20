421cffcbd26dafef5bf0f782d632b479e9f55009
sarii0213 <anjing.singka@gmail.com>
2023-01-20 22:23:04 +0900

    :+1: カテゴリーによってデフォルト画像を設定する処理をdecoratorに追加

--- app/decorators/item_decorator.rb
+++ app/decorators/item_decorator.rb
@@ -2,7 +2,42 @@ class ItemDecorator < Draper::Decorator
   delegate_all
 
   def img(size)
-    return 'dummy-100X100.png' unless image.attached?
+    unless image.attached?
+      # category id: name
+      # 1: 生活雑貨
+      # 2: ファッション小物
+      # 3: 美容グッズ
+      # 4: 洋服・布類
+      # 5: 紙類・書類
+      # 6: 書籍・CD/DVD
+      # 7: インテリア・電子機器
+      # 8: キッチン小物・食
+      # 9: スポーツ用品
+      # 10: デジタルデータ
+      default_image = case category.id
+                      when 1
+                        'spray.svg'
+                      when 2
+                        'hat.svg'
+                      when 3
+                        'lip_stick.svg'
+                      when 4
+                        'shirts.svg'
+                      when 5
+                        'docs.svg'
+                      when 6
+                        'book_cd.svg'
+                      when 7
+                        'side_table.svg'
+                      when 8
+                        'chopsticks.svg'
+                      when 9
+                        'racket.svg'
+                      when 10
+                        'pc.svg'
+                      end
+      return default_image
+    end
 
     command = case size
               when :thumb
