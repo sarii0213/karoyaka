baf9f3f22c84cd4a984a9b16ee1fd93c0d913a19
sarii0213 <anjing.singka@gmail.com>
2023-01-28 22:03:58 +0900

    :sparkles: #63 実績ページのコントローラとルーティングを作成
    コントローラで使いたい関連とクラスメソッド・そのスペックも追加

---
+++ app/controllers/achievements_controller.rb
@@ -0,0 +1,10 @@
+class AchievementsController < ApplicationController
+  def show
+    @days = current_user.items.distinct_days
+    @to_let_go_items_num = current_user.to_let_go_items.count
+    @done_letting_go_items_num = current_user.done_letting_go_items.count
+    @category_ranking = current_user.done_letting_go_items.joins(:category).group('categories.name').order(count_all: :desc).limit(3).count
+    @reason_ranking = current_user.done_letting_go_items.joins(:reason).group('reasons.name').order(count_all: :desc).limit(3).count
+    @way_ranking = current_user.done_letting_go_items.joins(:letting_go_way).group('letting_go_ways.name').order(count_all: :desc).limit(3).count
+  end
+end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -32,4 +32,16 @@ RSpec.describe Item do
   subject { build(:item) }
 
   it { is_expected.to be_valid }
+
+  describe '.distinct_days' do
+    let!(:user) { create(:user) }
+    before do
+      create(:item, user: user, created_at: Time.new(2023, 1, 28, 20, 0, 0))
+      create(:item, user: user, created_at: Time.new(2023, 1, 28, 20, 30, 0))
+      create(:item, user: user, created_at: Time.new(2023, 1, 30, 8, 0, 0))
+    end
+    it 'アイテムを登録した日数を取得できること' do
+      expect(user.items.distinct_days).to eq(2)
+    end
+  end
 end
