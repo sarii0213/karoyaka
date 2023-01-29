9e7829d744e9fa6cb5c6049c914c76f24a2905dc
sarii0213 <anjing.singka@gmail.com>
2023-01-29 16:04:52 +0900

    :shirt: #63 exec rubocop
    ABC size の警告を解消

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,12 +1,34 @@
 class AchievementsController < ApplicationController
   def show
+    set_days
+    set_items_num
+    set_category_ranking
+    set_reason_ranking
+    set_way_ranking
+  end
+
+  private
+
+  def set_days
     @days = current_user.items.distinct_days
+  end
+
+  def set_items_num
     @to_let_go_items_num = current_user.to_let_go_items.count
     @done_letting_go_items_num = current_user.done_letting_go_items.count
+  end
+
+  def set_category_ranking
     @category_ranking = current_user.done_letting_go_items.joins(:category)
                                     .group('categories.name').order(count_all: :desc).limit(3).count
+  end
+
+  def set_reason_ranking
     @reason_ranking = current_user.done_letting_go_items.joins(:reason)
                                   .group('reasons.name').order(count_all: :desc).limit(3).count
+  end
+
+  def set_way_ranking
     @way_ranking = current_user.done_letting_go_items.joins(:letting_go_way)
                                .group('letting_go_ways.name').order(count_all: :desc).limit(3).count
   end
