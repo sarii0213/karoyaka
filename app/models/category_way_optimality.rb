61955b48be55e0602384686baf8d7030d1f9f9f3
sarii0213 <anjing.singka@gmail.com>
2023-02-04 12:44:46 +0900

    :heavy_plus_sign: #67 カテゴリーごとの手放す方法の最適値モデルを作成

---
+++ app/models/category_way_optimality.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: category_way_optimalities
+#
+#  id                :bigint           not null, primary key
+#  score             :decimal(3, 2)
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  category_id       :bigint           not null
+#  letting_go_way_id :bigint           not null
+#
+# Indexes
+#
+#  category_way_id_uniqueness                            (category_id,letting_go_way_id) UNIQUE
+#  index_category_way_optimalities_on_category_id        (category_id)
+#  index_category_way_optimalities_on_letting_go_way_id  (letting_go_way_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#  fk_rails_...  (letting_go_way_id => letting_go_ways.id)
+#
+class CategoryWayOptimality < ApplicationRecord
+  belongs_to :category
+  belongs_to :letting_go_way
+end
