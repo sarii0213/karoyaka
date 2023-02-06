9920691b5c009fba3a8478dde4b19f04fa6a69e8
sarii0213 <anjing.singka@gmail.com>
2023-02-06 18:22:24 +0900

    :white_check_mark: #67 手放す方法のヒント表示機能のspecを作成

--- spec/system/to_let_go_items_spec.rb
+++ spec/system/to_let_go_items_spec.rb
@@ -17,7 +17,23 @@ RSpec.describe '手放したいものリスト' do
       click_on '登録する'
       expect(page).to have_content '登録しました'
     end
+
+    it '手放したいもののカテゴリー・理由を選択すると手放す方法のヒントが表示されること' do
+      visit new_to_let_go_item_path
+      select '生活雑貨', from: 'to_let_go_item[category_id]'
+      select '使ってない', from: 'to_let_go_item[reason_id]'
+      # 手放す方法：「ゴミ」, 「お店」, 「ゆずる」, 「回収依頼」
+      # カテゴリー「生活雑貨」の最適値： 1, 0.3, 0.5, 0.4
+      # 理由「使ってない」の最適値：0.7, 0.7, 0.7, 0.5
+      # カテゴリー x 理由：0.7, 0.21, 0.35, 0.2
+      within('#letting_go_way_hint') do
+        expect(page).to have_content('ゴミ')
+        expect(page).to have_content('ゆずる')
+        expect(page).to have_content('お店')
+      end
+    end
   end
+
 
   describe '編集' do
     let!(:to_let_go_item) { create(:to_let_go_item, user:, category_id: 2) }
