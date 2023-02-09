3da6c741490036b398bd3a814b6e0f052396587b
sarii0213 <anjing.singka@gmail.com>
2023-02-09 12:55:52 +0900

    :shirt: #69 exec rspec

--- app/controllers/mypage/accounts_controller.rb
+++ app/controllers/mypage/accounts_controller.rb
@@ -2,7 +2,7 @@ class Mypage::AccountsController < ApplicationController
   def show
     @user = current_user
     @days = current_user.items.distinct_days
-    @day_what = if current_user.items.last.created_at.day == Time.now.day
+    @day_what = if current_user.items.present? && current_user.items.last.created_at.day == Time.now.day
                   @days
                 else
                   @days + 1
