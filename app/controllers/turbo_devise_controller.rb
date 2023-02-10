411f54a15a306d6c3e878d9e2da7e6acece7f10d
sarii0213 <anjing.singka@gmail.com>
2023-02-11 00:43:16 +0900

    :shirt: #71 exec rubocop

--- app/controllers/turbo_devise_controller.rb
+++ app/controllers/turbo_devise_controller.rb
@@ -1,18 +1,20 @@
+# rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/PerceivedComplexity
 class TurboDeviseController < ApplicationController
   class Responder < ActionController::Responder
-    def to_turbo_stream # to handle turbo stream format
+    def to_turbo_stream
+      # to handle turbo stream format
       if @default_response
         @default_response.call(options.merge(formats: :html))
       else
         controller.render(options.merge(formats: :html))
         # tell the responder to render with html format & no need to look for turbo stream template
       end
-    rescue ActionView::MissingTemplate => error # handle the case when no template being found
-      if get? # if it's get request, just reraise the error
-        raise error
-      elsif has_errors? && default_action
-      # has_errors?: for html, js, json responses. tell the responder to treat as html request  (responders gem)
-      # default_action: { post: :new, patch/put: :edit } (responders gem)
+    rescue ActionView::MissingTemplate => e # handle the case when no template being found
+      raise e if get? # if it's get request, just reraise the error
+
+      if has_errors? && default_action
+        # has_errors?: for html, js, json responses. tell the responder to treat as html request  (responders gem)
+        # default_action: { post: :new, patch/put: :edit } (responders gem)
         if respond_to?(:error_rendering_options, true)
           # For responders 3.1.0 or higher
           render error_rendering_options.merge(formats: :html, status: :unprocessable_entity)
@@ -21,7 +23,7 @@ class TurboDeviseController < ApplicationController
           # add formats & status value to the rendering options
         end
       else
-        navigation_behavior error # redirect to index page
+        navigation_behavior e # redirect to index page
       end
     end
   end
@@ -29,3 +31,5 @@ class TurboDeviseController < ApplicationController
   self.responder = Responder # assign Responder class written above to be the responder
   respond_to :html, :turbo_stream # tell the responder which formats we want to respond with
 end
+
+# rubocop:enable Metrics/AbcSize, Metrics/MethodLength, Metrics/PerceivedComplexity
