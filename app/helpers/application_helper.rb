bfd4438ac019472dfb5a79c4fb2058ce51752736
sarii0213 <anjing.singka@gmail.com>
2023-02-20 22:44:09 +0900

    :shirt: #80 exec rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,15 +7,21 @@ module ApplicationHelper
       description: Settings.meta.description,
       keywords: Settings.meta.keywords,
       canonical: request.original_url,
-      og: {
-        title: :full_title,
-        type: Settings.meta.og.type,
-        url: request.original_url,
-        image: image_url(Settings.meta.og.image_path),
-        site_name: :site,
-        description: :description,
-        locale: 'ja_JP'
-      }
+      og: default_og
+    }
+  end
+
+  private
+
+  def default_og
+    {
+      title: :full_title,
+      type: Settings.meta.og.type,
+      url: request.original_url,
+      image: image_url(Settings.meta.og.image_path),
+      site_name: :site,
+      description: :description,
+      locale: 'ja_JP'
     }
   end
 end
