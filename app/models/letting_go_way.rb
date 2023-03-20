ad8ec11da568cdcc32895471e71a012f05f5e9da
sarii0213 <anjing.singka@gmail.com>
2023-03-20 11:59:15 +0900

    :recycle: #110 コントローラからモデルに処理を移す

--- app/models/letting_go_way.rb
+++ app/models/letting_go_way.rb
@@ -18,4 +18,27 @@ class LettingGoWay < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
+
+  has_many :category_way_optimalities
+  has_many :reason_way_optimalities
+
+  def self.optimal(category_id, reason_id)
+    category_scores = CategoryWayOptimality.scores(category_id)
+    reason_scores = ReasonWayOptimality.scores(reason_id)
+    scores = [category_scores, reason_scores].transpose.map { |ary| ary.inject(:*) }
+    way_ids = []
+    3.times do |_|
+      way_ids.push(scores.index(scores.max) + 1)
+      scores[scores.index(scores.max)] = 0
+    end
+    where(id: way_ids)
+  end
+
+  def self.category_optimal(category_id)
+    joins(:category_way_optimalities).merge(CategoryWayOptimality.where(category_id:).order(score: :desc)).limit(3)
+  end
+
+  def self.reason_optimal(reason_id)
+    joins(:reason_way_optimalities).merge(ReasonWayOptimality.where(reason_id:).order(score: :desc)).limit(3)
+  end
 end
