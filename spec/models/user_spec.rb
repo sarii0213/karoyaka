2540339ef3946ab68d7100e8a0e53946ae42c594
sarii0213 <anjing.singka@gmail.com>
2023-03-23 22:17:50 +0900

    :white_check_mark: #116 追加したモデルメソッドのスペックを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -55,4 +55,40 @@ RSpec.describe User do
       expect(user.favorite?(quote_2)).to be false
     end
   end
+
+  describe '#category_ranking' do
+    let!(:user) { create(:user) }
+    before do
+      create_list(:done_letting_go_item, 3, user: user, category_id: 1)
+      create_list(:done_letting_go_item, 2, user: user, category_id: 2)
+      create_list(:done_letting_go_item, 1, user: user, category_id: 3)
+    end
+    it '手放し済みアイテムのカテゴリー別ランキングを取得できること' do
+      expect(user.category_ranking).to eq({Category.find(1).name => 3, Category.find(2).name => 2, Category.find(3).name => 1})
+    end
+  end
+
+  describe '#reason_ranking' do
+    let!(:user) { create(:user) }
+    before do
+      create_list(:done_letting_go_item, 3, user: user, reason_id: 1)
+      create_list(:done_letting_go_item, 2, user: user, reason_id: 2)
+      create_list(:done_letting_go_item, 1, user: user, reason_id: 3)
+    end
+    it '手放し済みアイテムの理由別ランキングを取得できること' do
+      expect(user.reason_ranking).to eq({Reason.find(1).name => 3, Reason.find(2).name => 2, Reason.find(3).name => 1})
+    end
+  end
+
+  describe '#way_ranking' do
+    let!(:user) { create(:user) }
+    before do
+      create_list(:done_letting_go_item, 3, user: user, letting_go_way_id: 1)
+      create_list(:done_letting_go_item, 2, user: user, letting_go_way_id: 2)
+      create_list(:done_letting_go_item, 1, user: user, letting_go_way_id: 3)
+    end
+    it '手放し済みアイテムの手放す方法別ランキングを取得できること' do
+      expect(user.way_ranking).to eq({LettingGoWay.find(1).name => 3, LettingGoWay.find(2).name => 2, LettingGoWay.find(3).name => 1})
+    end
+  end
 end
